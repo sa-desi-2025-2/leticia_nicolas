11c8900f328592f210448e3f10ba517718126a99
Letícia-j-prochnow <leticia_j_prochnow@estudante.sesisenai.org.br>
2025-10-30 16:20:38 -0300

    teste

--- php/cadastro.php
+++ php/cadastro.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro de Usuário</title>
+    <title>Cadastro de Usuário </title>
     <link rel="stylesheet" href="../css/cadastro.css">
 </head>
 <body>
